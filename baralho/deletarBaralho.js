a08e548beea66e7c4fd689c1184de5391b9b8fbe
bianekkarina <bianekkarina@gmail.com>
2024-08-19 15:45:36 -0300

    mudei o deletar baralhos

--- baralho/deletarBaralho.js
+++ baralho/deletarBaralho.js
@@ -1,9 +1,15 @@
-const { baralhos } = require('../dados.js')
+const { baralhos, flashcards } = require('../dados.js')
 
 function deletarBaralho(id) {
     let index = baralhos.findIndex(baralho => baralho.id === id) 
     if (index !== -1) {
        baralhos.splice(index, 1)
+    
+        for (let i = flashcards.length -1; i >= 0; i--) {
+            if (flashcards[i].idBaralho === id) {
+                flashcards.splice(i, 1)
+            }
+       }
     }
 }
 
